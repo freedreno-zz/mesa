a3ea9edfdadb63e107f3a52bda2006b60d49355c
Brian Paul <brian.paul@tungstengraphics.com>
1999-11-22 22:19:50 +0000

    removed GL_EXT_get_proc_address from ext strings

--- src/mesa/drivers/x11/fakeglx.c
+++ src/mesa/drivers/x11/fakeglx.c
@@ -1,4 +1,4 @@
-/* $Id: fakeglx.c,v 1.12.2.1 1999/11/18 15:16:03 brianp Exp $ */
+/* $Id: fakeglx.c,v 1.12.2.2 1999/11/22 22:19:50 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -1430,10 +1430,10 @@ static const char *get_extensions( void )
 #ifdef FX
    const char *fx = getenv("MESA_GLX_FX");
    if (fx && fx[0] != 'd') {
-      return "GLX_MESA_pixmap_colormap GLX_EXT_visual_info GLX_MESA_release_buffers GLX_MESA_copy_sub_buffer GLX_SGI_video_sync GLX_EXT_get_proc_address GLX_MESA_set_3dfx_mode";
+      return "GLX_MESA_pixmap_colormap GLX_EXT_visual_info GLX_MESA_release_buffers GLX_MESA_copy_sub_buffer GLX_SGI_video_sync GLX_MESA_set_3dfx_mode";
    }
 #endif
-   return "GLX_MESA_pixmap_colormap GLX_EXT_visual_info GLX_MESA_release_buffers GLX_MESA_copy_sub_buffer GLX_SGI_video_sync GL_EXT_get_proc_address";
+   return "GLX_MESA_pixmap_colormap GLX_EXT_visual_info GLX_MESA_release_buffers GLX_MESA_copy_sub_buffer GLX_SGI_video_sync";
 }
 
 
