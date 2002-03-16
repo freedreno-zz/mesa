cef9cdf3bb17d5a84c3028c5b6de1ff6b6d558ae
Brian Paul <brian.paul@tungstengraphics.com>
2002-03-16 01:00:43 +0000

    undo previous check-in

--- src/mesa/main/glheader.h
+++ src/mesa/main/glheader.h
@@ -1,4 +1,4 @@
-/* $Id: glheader.h,v 1.23.2.2 2002/03/16 00:50:12 brianp Exp $ */
+/* $Id: glheader.h,v 1.23.2.3 2002/03/16 01:00:43 brianp Exp $ */
 
 /*
  * Mesa 3-D graphics library
@@ -260,5 +260,13 @@ typedef struct tagPIXELFORMATDESCRIPTOR PIXELFORMATDESCRIPTOR, *PPIXELFORMATDESC
  */
 typedef union { GLfloat f; GLint i; } fi_type;
 
+
+                                                       
+#ifndef GL_MIRRORED_REPEAT_ARB
+#define GL_MIRRORED_REPEAT_ARB  0x8370
+#endif
+#ifndef GL_ARB_texture_mirrored_repeat
+#define GL_ARB_texture_mirrored_repeat 1
+#endif
 
 #endif /* GLHEADER_H */
