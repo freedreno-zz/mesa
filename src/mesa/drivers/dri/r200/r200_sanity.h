ed1b6e98e29f597948a9563adf781632123482f2
Keith Whitwell <keith@tungstengraphics.com>
2003-03-10 11:23:04 +0000

    r200 driver, brought over by Jon Smirl

---
+++ src/mesa/drivers/dri/r200/r200_sanity.h
@@ -0,0 +1,11 @@
+#ifndef R200_SANITY_H
+#define R200_SANITY_H
+
+extern int r200SanityCmdBuffer( r200ContextPtr rmesa,
+				int nbox,
+				XF86DRIClipRectRec *boxes );
+
+
+extern int r200ValidateTexObjs( r200ContextPtr rmesa );
+
+#endif
