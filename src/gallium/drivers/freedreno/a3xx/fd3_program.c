58b3ee49041757904ddc18768f847e0747da49a3
Rob Clark <robclark@freedesktop.org>
2013-05-01 17:38:20 -0400

    XXX more a3xx

--- src/gallium/drivers/freedreno/a3xx/fd3_program.c
+++ src/gallium/drivers/freedreno/a3xx/fd3_program.c
@@ -154,9 +154,153 @@ TODO
 */
 }
 
+/* once the compiler is good enough, we should construct TGSI in the
+ * core freedreno driver, and then let the a2xx/a3xx parts compile
+ * the internal shaders from TGSI the same as regular shaders.  This
+ * would be the first step towards handling most of clear (and the
+ * gmem<->mem blits) from the core via normal state changes and shader
+ * state objects.
+ *
+ * (Well, there would still be some special bits, because there are
+ * some registers that don't get set for normal draw, but this should
+ * be relatively small and could be handled via callbacks from core
+ * into a2xx/a3xx..)
+ */
+static struct fd3_shader_stateobj *
+create_internal_shader(enum shader_t type, struct ir3_shader *ir)
+{
+	struct fd3_shader_stateobj *so = CALLOC_STRUCT(fd3_shader_stateobj);
+
+	if (!so)
+		return NULL;
+
+	so->type = type;
+	so->ir = ir;
+
+	so->bin = ir3_shader_assemble(so->ir, &so->info);
+	assert(so->bin);
+
+	return so;
+}
+
+/* Creates shader:
+ *    (sy)(ss)(rpt1)bary.f (ei)r0.z, (r)0, r0.x
+ *    (rpt5)nop
+ *    sam (f32)(xyzw)r0.x, r0.z, s#0, t#0
+ *    (sy)(rpt3)cov.f32f16 hr0.x, (r)r0.x
+ *    end
+ */
+static struct fd3_shader_stateobj *
+create_blit_fp(void)
+{
+	struct ir3_shader *ir = ir3_shader_create();
+	struct ir3_instruction *instr;
+
+	/* (sy)(ss)(rpt1)bary.f (ei)r0.z, (r)0, r0.x */
+	instr = ir3_instr_create(ir, 2, OPC_BARY_F);
+	instr->flags = IR3_INSTR_SY | IR3_INSTR_SS;
+	instr->repeat = 1;
+
+	ir3_reg_create(instr, regid(0,2), IR3_REG_HALF |  /* (ei)r0.z */
+			IR3_REG_EI);
+	ir3_reg_create(instr, 0, IR3_REG_R |              /* (r)0 */
+			IR3_REG_IMMED)->iim_val = 0;
+	ir3_reg_create(instr, regid(0,0), 0);             /* r0.x */
+
+	/* (rpt5)nop */
+	instr = ir3_instr_create(ir, 0, OPC_NOP);
+	instr->repeat = 5;
+
+	/* sam (f32)(xyzw)r0.x, r0.z, s#0, t#0 */
+	instr = ir3_instr_create(ir, 5, OPC_SAM);
+	instr->cat5.samp = 0;
+	instr->cat5.tex  = 0;
+	instr->cat5.type = TYPE_F32;
+
+	ir3_reg_create(instr, regid(0,0),                 /* (xyzw)r0.x */
+			0)->wrmask = 0xf;
+	ir3_reg_create(instr, regid(0,2), 0);             /* r0.z */
+
+	/* (sy)(rpt3)cov.f32f16 hr0.x, (r)r0.x */
+	instr = ir3_instr_create(ir, 1, 0);  /* mov/cov instructions have no opc */
+	instr->flags = IR3_INSTR_SY;
+	instr->repeat = 3;
+	instr->cat1.src_type = TYPE_F32;
+	instr->cat1.dst_type = TYPE_F16;
+
+	ir3_reg_create(instr, regid(0,0), IR3_REG_HALF);  /* hr0.x */
+	ir3_reg_create(instr, regid(0,0), IR3_REG_R);     /* (r)r0.x */
+
+	/* end */
+	instr = ir3_instr_create(ir, 0, OPC_END);
+
+	return create_internal_shader(SHADER_FRAGMENT, ir);
+}
+
+/* Creates shader:
+ *    (sy)(ss)end
+ */
+static struct fd3_shader_stateobj *
+create_blit_vp(void)
+{
+	struct ir3_shader *ir = ir3_shader_create();
+	struct ir3_instruction *instr;
+
+	/* (sy)(ss)end */
+	instr = ir3_instr_create(ir, 0, OPC_END);
+	instr->flags = IR3_INSTR_SY | IR3_INSTR_SS;
+
+	return create_internal_shader(SHADER_VERTEX, ir);
+}
+
+/* Creates shader:
+ *    (sy)(ss)(rpt3)mov.f16f16 hr0.x, (r)hc0.x
+ *    end
+ */
+static struct fd3_shader_stateobj *
+create_solid_fp(void)
+{
+	struct ir3_shader *ir = ir3_shader_create();
+	struct ir3_instruction *instr;
+
+	/* (sy)(ss)(rpt3)mov.f16f16 hr0.x, (r)hc0.x */
+	instr = ir3_instr_create(ir, 1, 0);  /* mov/cov instructions have no opc */
+	instr->flags = IR3_INSTR_SY | IR3_INSTR_SS;
+	instr->repeat = 3;
+	instr->cat1.src_type = TYPE_F16;
+	instr->cat1.dst_type = TYPE_F16;
+
+	ir3_reg_create(instr, regid(0,0), IR3_REG_HALF);  /* hr0.x */
+	ir3_reg_create(instr, regid(0,0), IR3_REG_HALF |  /* (r)hc0.x */
+			IR3_REG_CONST | IR3_REG_R);
+
+	/* end */
+	instr = ir3_instr_create(ir, 0, OPC_END);
+
+	return create_internal_shader(SHADER_FRAGMENT, ir);
+}
+
+/* Creates shader:
+ *    (sy)(ss)end
+ */
+static struct fd3_shader_stateobj *
+create_solid_vp(void)
+{
+	struct ir3_shader *ir = ir3_shader_create();
+	struct ir3_instruction *instr;
+
+	/* (sy)(ss)end */
+	instr = ir3_instr_create(ir, 0, OPC_END);
+	instr->flags = IR3_INSTR_SY | IR3_INSTR_SS;
+
+	return create_internal_shader(SHADER_VERTEX, ir);
+}
+
 void
 fd3_prog_init(struct pipe_context *pctx)
 {
+	struct fd_context *ctx = fd_context(pctx);
+
 	pctx->create_fs_state = fd_fp_state_create;
 	pctx->bind_fs_state = fd_fp_state_bind;
 	pctx->delete_fs_state = fd_fp_state_delete;
@@ -164,6 +308,11 @@ fd3_prog_init(struct pipe_context *pctx)
 	pctx->create_vs_state = fd_vp_state_create;
 	pctx->bind_vs_state = fd_vp_state_bind;
 	pctx->delete_vs_state = fd_vp_state_delete;
+
+	ctx->solid_prog.fp = create_solid_fp();
+	ctx->solid_prog.vp = create_solid_vp();
+	ctx->blit_prog.fp = create_blit_fp();
+	ctx->blit_prog.vp = create_blit_vp();
 }
 
 void
