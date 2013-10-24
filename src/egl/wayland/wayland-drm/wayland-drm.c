aras-p/glsl-optimizer

wayland: Don't rely on static variable for identifying wl_drm buffers
Now that libEGL has been fixed to not leak all kinds of symbols, gbm
links to its own copy of the libwayland-drm.a helper library.  That means
we can't rely on comparing the addresses of a static vtable symbol in that
library to determine if a wl_buffer is a wl_drm_buffer.  Instead, we
move the vtable into the wl_drm struct and use that for comparing.

https://bugs.freedesktop.org/show_bug.cgi?id=69437

Cc: 9.2 <mesa-stable@lists.freedesktop.org>

src/egl/wayland/wayland-drm/wayland-drm.c
@@ -47,6 +47,8 @@ struct wl_drm {
         uint32_t flags;
 
 	struct wayland_drm_callbacks *callbacks;
+
+        struct wl_buffer_interface buffer_interface;
 };
 
 static void
@@ -64,10 +66,6 @@ buffer_destroy(struct wl_client *client, struct wl_resource *resource)
 {
 	wl_resource_destroy(resource);
 }
-
-const static struct wl_buffer_interface drm_buffer_interface = {
-	buffer_destroy
-};
 
 static void
 create_buffer(struct wl_client *client, struct wl_resource *resource,
@@ -115,7 +113,7 @@ create_buffer(struct wl_client *client, struct wl_resource *resource,
 	}
 
 	wl_resource_set_implementation(buffer->resource,
-				       (void (**)(void)) &drm_buffer_interface,
+				       (void (**)(void)) &drm->buffer_interface,
 				       buffer, destroy_buffer);
 }
 
@@ -243,15 +241,17 @@ bind_drm(struct wl_client *client, void *data, uint32_t version, uint32_t id)
 }
 
 struct wl_drm_buffer *
-wayland_drm_buffer_get(struct wl_resource *resource)
+wayland_drm_buffer_get(struct wl_drm *drm, struct wl_resource *resource)
 {
+	struct wl_drm_buffer *buffer;
+
 	if (resource == NULL)
 		return NULL;
 
-	if (wl_resource_instance_of(resource, &wl_buffer_interface,
-				    &drm_buffer_interface))
+        if (wl_resource_instance_of(resource, &wl_buffer_interface,
+                                    &drm->buffer_interface))
 		return wl_resource_get_user_data(resource);
-	else
+        else
 		return NULL;
 }
 
@@ -269,6 +269,8 @@ wayland_drm_init(struct wl_display *display, char *device_name,
 	drm->callbacks = callbacks;
 	drm->user_data = user_data;
         drm->flags = flags;
+
+        drm->buffer_interface.destroy = buffer_destroy;
 
 	wl_global_create(display, &wl_drm_interface, 2, drm, bind_drm);
 

src/egl/wayland/wayland-drm/wayland-drm.h
@@ -92,7 +92,7 @@ struct wayland_drm_callbacks {
 enum { WAYLAND_DRM_PRIME = 0x01 };
 
 struct wl_drm_buffer *
-wayland_drm_buffer_get(struct wl_resource *resource);
+wayland_drm_buffer_get(struct wl_drm *drm, struct wl_resource *resource);
 
 struct wl_drm *
 wayland_drm_init(struct wl_display *display, char *device_name,

freedreno-zz/mesa

Backported from 360a141f24a9d00891665b7fedb77ffb116944ca.

src/egl/wayland/wayland-drm/wayland-drm.c
@@ -45,6 +45,7 @@ struct wl_drm {
         uint32_t flags;
 
 	struct wayland_drm_callbacks *callbacks;
+        struct wl_buffer_interface buffer_interface;
 };
 
 static void
@@ -62,10 +63,6 @@ buffer_destroy(struct wl_client *client, struct wl_resource *resource)
 {
 	wl_resource_destroy(resource);
 }
-
-const static struct wl_buffer_interface drm_buffer_interface = {
-	buffer_destroy
-};
 
 static void
 create_buffer(struct wl_client *client, struct wl_resource *resource,
@@ -107,7 +104,7 @@ create_buffer(struct wl_client *client, struct wl_resource *resource,
 	buffer->buffer.resource.object.id = id;
 	buffer->buffer.resource.object.interface = &wl_buffer_interface;
 	buffer->buffer.resource.object.implementation =
-		(void (**)(void)) &drm_buffer_interface;
+		(void (**)(void)) &drm->buffer_interface;
 	buffer->buffer.resource.data = buffer;
 
 	buffer->buffer.resource.destroy = destroy_buffer;
@@ -246,6 +243,7 @@ wayland_drm_init(struct wl_display *display, char *device_name,
 	drm->callbacks = callbacks;
 	drm->user_data = user_data;
         drm->flags = flags;
+        drm->buffer_interface.destroy = buffer_destroy;
 
 	wl_display_add_global(display, &wl_drm_interface, drm, bind_drm);
 
@@ -263,10 +261,10 @@ wayland_drm_uninit(struct wl_drm *drm)
 }
 
 int
-wayland_buffer_is_drm(struct wl_buffer *buffer)
+wayland_buffer_is_drm(struct wl_drm *drm, struct wl_buffer *buffer)
 {
 	return buffer->resource.object.implementation == 
-		(void (**)(void)) &drm_buffer_interface;
+		(void (**)(void)) &drm->buffer_interface;
 }
 
 uint32_t

src/egl/wayland/wayland-drm/wayland-drm.h
@@ -99,7 +99,7 @@ void
 wayland_drm_uninit(struct wl_drm *drm);
 
 int
-wayland_buffer_is_drm(struct wl_buffer *buffer);
+wayland_buffer_is_drm(struct wl_drm *drm, struct wl_buffer *buffer);
 
 uint32_t
 wayland_drm_buffer_get_format(struct wl_buffer *buffer_base);