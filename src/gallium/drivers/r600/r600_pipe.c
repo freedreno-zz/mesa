a02c50ef4e9fa1d2bbb5d8ef05ef67b9d74ed075
Tom Stellard <thomas.stellard@amd.com>
2014-02-24 16:51:05 -0500

    r600g/compute: PIPE_CAP_COMPUTE should be false for pre-evergreen GPUs
    This prevents clover from using unsupported devices.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    
    CC: "10.0 10.1" <mesa-stable@lists.freedesktop.org>
    (cherry picked from commit f61e382f0afc48bc09f21c50639f760acca85bc5)

--- src/gallium/drivers/r600/r600_pipe.c
+++ src/gallium/drivers/r600/r600_pipe.c
@@ -413,7 +413,6 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_VERTEX_ELEMENT_SRC_OFFSET_4BYTE_ALIGNED_ONLY:
 	case PIPE_CAP_USER_INDEX_BUFFERS:
 	case PIPE_CAP_USER_CONSTANT_BUFFERS:
-	case PIPE_CAP_COMPUTE:
 	case PIPE_CAP_START_INSTANCE:
 	case PIPE_CAP_MAX_DUAL_SOURCE_RENDER_TARGETS:
 	case PIPE_CAP_TEXTURE_BUFFER_OBJECTS:
@@ -421,6 +420,9 @@ static int r600_get_param(struct pipe_screen* pscreen, enum pipe_cap param)
 	case PIPE_CAP_QUERY_PIPELINE_STATISTICS:
 	case PIPE_CAP_TEXTURE_MULTISAMPLE:
 		return 1;
+
+	case PIPE_CAP_COMPUTE:
+		return rscreen->b.chip_class > R700;
 
 	case PIPE_CAP_TGSI_TEXCOORD:
 		return 0;
