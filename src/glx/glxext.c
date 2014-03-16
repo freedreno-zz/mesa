498853b9fdd21fad049f49d73002d5d7bac93092
Emil Velikov <emil.l.velikov@gmail.com>
2014-03-16 02:56:18 +0000

    glx: drop obsolete _XUnlock_Mutex in __glXInitialize error path
    With commit 1f1928db001(glx: Drop _Xglobal_lock while we create and
    initialize glx display) we've split the big _Xglobal_lock handling in
    a more fine grained manner.
    
    Unfortunatelly we forgot to drop the unlock_mutex on the error paths,
    leading to undefined behaviour as the mutex is already unlocked.
    
    Cc: Kristian Høgsberg <krh@bitplanet.net>
    Cc: "9.2 10.0 10.1"  <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    (cherry picked from commit f9832f960fa8edcee0eb6866698cc5f9f25bd8f9)

--- src/glx/glxext.c
+++ src/glx/glxext.c
@@ -823,7 +823,6 @@ __glXInitialize(Display * dpy)
    dpyPriv->codes = XInitExtension(dpy, __glXExtensionName);
    if (!dpyPriv->codes) {
       free(dpyPriv);
-      _XUnlockMutex(_Xglobal_lock);
       return NULL;
    }
 
@@ -839,7 +838,6 @@ __glXInitialize(Display * dpy)
 		     &dpyPriv->majorVersion, &dpyPriv->minorVersion)
        || (dpyPriv->majorVersion == 1 && dpyPriv->minorVersion < 1)) {
       free(dpyPriv);
-      _XUnlockMutex(_Xglobal_lock);
       return NULL;
    }
 
@@ -900,7 +898,7 @@ __glXInitialize(Display * dpy)
    dpyPriv->next = glx_displays;
    glx_displays = dpyPriv;
 
-    _XUnlockMutex(_Xglobal_lock);
+   _XUnlockMutex(_Xglobal_lock);
 
    return dpyPriv;
 }
