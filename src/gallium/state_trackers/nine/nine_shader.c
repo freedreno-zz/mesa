41ca03a7b442b642a6768676dd6dede2a7f8edc5
Xavier Bouchoux <xavierb@gmail.com>
2014-12-17 23:10:04 +0100

    st/nine: Fix D3DRS_POINTSPRITE support
    It's done by testing the existence of the point sprite output register *after* parsing the vertex shader.
    
    Reviewed-by: Ilia Mirkin <imirkin@alum.mit.edu>
    Reviewed-by: David Heidelberg <david@ixit.cz>
    Reviewed-by: Axel Davy <axel.davy@ens.fr>
    Signed-off-by: Xavier Bouchoux <xavierb@gmail.com>
    
    Cc: "10.4" <mesa-stable@lists.freedesktop.org>
    (cherry picked from commit dc8898918900a584b1783c99e77b6310f3834f11)

--- src/gallium/state_trackers/nine/nine_shader.c
+++ src/gallium/state_trackers/nine/nine_shader.c
@@ -2844,9 +2844,6 @@ nine_translate_shader(struct NineDevice9 *device, struct nine_shader_info *info)
             ureg_property_fs_coord_pixel_center(tx->ureg, TGSI_FS_COORD_PIXEL_CENTER_INTEGER);
     }
 
-    if (!ureg_dst_is_undef(tx->regs.oPts))
-        info->point_size = TRUE;
-
     while (!sm1_parse_eof(tx))
         sm1_parse_instruction(tx);
     tx->parse++; /* for byte_size */
@@ -2861,6 +2858,9 @@ nine_translate_shader(struct NineDevice9 *device, struct nine_shader_info *info)
         ureg_property_vs_window_space_position(tx->ureg, TRUE);
 
     ureg_END(tx->ureg);
+
+    if (IS_VS && !ureg_dst_is_undef(tx->regs.oPts))
+        info->point_size = TRUE;
 
     if (debug_get_bool_option("NINE_TGSI_DUMP", FALSE)) {
         unsigned count;
