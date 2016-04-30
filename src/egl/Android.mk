a5d3d3475cc5aaafa0eef2c89ff80d83b69687df
Rob Herring <robh@kernel.org>
2016-04-30 14:37:15 -0500

    Android: remove the linking dependency on libgralloc_drm
    We only depend on the headers now.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- src/egl/Android.mk
+++ src/egl/Android.mk
@@ -46,6 +46,7 @@ LOCAL_CFLAGS := \
 LOCAL_C_INCLUDES := \
 	$(MESA_TOP)/src/egl/main \
 	$(MESA_TOP)/src/egl/drivers/dri2 \
+	external/drm_gralloc
 
 LOCAL_STATIC_LIBRARIES := \
 	libmesa_loader
@@ -55,7 +56,6 @@ LOCAL_SHARED_LIBRARIES := \
 	libhardware \
 	liblog \
 	libcutils \
-	libgralloc_drm \
 
 ifeq ($(shell echo "$(MESA_ANDROID_VERSION) >= 4.2" | bc),1)
 LOCAL_SHARED_LIBRARIES += libsync
